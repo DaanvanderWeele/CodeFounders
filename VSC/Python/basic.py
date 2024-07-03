2186459326d772cb32292a9e40efaa92035148e9
Werk <werk@Air-van-Daan-2.home>
2024-07-03 16:22:12 +0200

    First commit

---
+++ VSC/Python/basic.py
@@ -0,0 +1,19 @@
+# Comment
+
+# Variabele
+a = 5
+
+print("Hello World!")
+
+# Functie definitie
+def function1():
+    print("Functie 1 print dit")
+
+def function2(tekst):
+    print("Ik print " + tekst)
+
+# Functie aanroepen
+function1()
+function2("Daan")
+
+
---
+++ VSC/Python/read_csv.py
@@ -0,0 +1,12 @@
+print("start uitlezen CSV") #kijken of de file werkt
+
+import pandas #library voor lezen
+
+#pandas gebruiken om csv uit te lezen en in een dataframe te zetten
+df = pandas.read_csv('/Users/werk/Desktop/CodeFounders/VSC/Python/pokemon.csv') #als deze file ergens anders staat volledig path invullen
+
+print(df["Name"])
+
+for r,rij in df.iterrows(): # for loop
+    print(rij["Name"])
+    #print(rij["Attack"])
